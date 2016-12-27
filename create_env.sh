f656901ba96eb8094f4d33849358117fb930a24e
Edwin Marte Torres <kdgpro15@gmail.com>
2016-12-27 17:48:25 -0400

    /refactor 'Changing to Python3 and Introducing Python Decouple'

--- create_env.sh
+++ create_env.sh
@@ -2,14 +2,14 @@
 echo "\n***** Installing *****"
 echo "/* Installing Python, Pip and VirtualEnv */"
 sudo apt-get update
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get install python3-pip python3-dev build-essential
 pip install virtualenv virtualenvwrapper
 pip install --upgrade pip
 echo "/* Installing PostgreSql */"
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 echo "\n***** Creating and Activating Virtual Environment *****"
-virtualenv venv
+virtualenv -p python3 venv
 source venv/bin/activate
 
 echo "\n***** Installing Dependencies *****"
