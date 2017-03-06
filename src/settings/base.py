6aaaaf6488abc28e0a567d27e98f1e2a49ebef2d
Edwin Marte Torres <kdgpro15@gmail.com>
2017-03-06 01:53:26 +0000

    configs_fix/.. Updating Readme, Requirements and adding License

--- src/settings/base.py
+++ src/settings/base.py
@@ -136,12 +136,3 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
 }
-
-# Session Complements
-SECURE_SSL_HOST = config('SECURE_SSL_HOST', default='')
-SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', default=False, cast=bool)
-CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
-SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=0, cast=int)
-SECURE_HSTS_INCLUDE_SUBDOMAINS = config('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=False, cast=bool)
