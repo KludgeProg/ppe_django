3192625f78bc7c8b6d3f1ba032571662f3a3c3e9
Edwin Marte Torres <kdgpro15@gmail.com>
2017-03-15 20:07:25 -0400

    Release v1.1 (#10)
    * Creating Vagrant Configuration (#6)
    * Creating Vagrant files configurations
    * Creating Readme File
    * Configs fix (#8)
    * Updating Readme and configuring settings
    * Updating Readme and configuring requirements
    * Configuring Django_Debug_Toolbar
    * Refactoring Settings and configuring Debug Toolbar
    * Refactoring env.example and updating settings
    * Updating Readme, Requirements and adding License

---
+++ src/settings/production.py
@@ -0,0 +1,10 @@
+from .base import *
+
+# Session Complements
+SECURE_SSL_HOST = config('SECURE_SSL_HOST', default='')
+SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', default=False, cast=bool)
+CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
+SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=0, cast=int)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = config('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=False, cast=bool)
